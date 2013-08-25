400938d5a885fc31c8da13a2b96708df97fa3e93
elena <ele.wil@gmail.com>
2013-08-25 15:02:16 +0800

    Improving field comments.

--- contrib/abstract/basics/models.py
+++ contrib/abstract/basics/models.py
@@ -10,7 +10,10 @@ from contrib.abstract.basics.querysets import LiveQuerySet
 class AbstractAuthor(models.Model):
 
     user_created = models.ForeignKey(settings.AUTH_USER_MODEL, editable=False)
+    """User who first created this object (required)."""
+
     user_updated = models.ForeignKey(settings.AUTH_USER_MODEL, editable=False)
+    """User who last saved this object (required)."""
 
     class Meta(object):
         abstract = True
@@ -20,13 +23,16 @@ class AbstractAuthor(models.Model):
             if not self.pk:
                 self.user_created = user
             self.user_updated = user
-        return super(Author, self).save(**kwargs)
+        return super(AbstractAuthor, self).save(**kwargs)
 
 
 class AbstractTime(models.Model):
 
     time_created = models.DateTimeField(auto_now_add=True, editable=False)
+    """:py:class:`datetime` for when this object was first created."""
+
     time_updated = models.DateTimeField(auto_now=True, editable=False)
+    """:py:class:`datetime` for when this object was last saved."""
 
     class Meta(object):
         abstract = True
@@ -38,12 +44,16 @@ class AbstractLive(models.Model):
     is_featured = models.BooleanField('featured', db_index=True, default=False)
 
     time_publish = models.DateTimeField(db_index=True, default=timezone.now)
+    """:py:class:`datetime` for when this object was published."""
+
     user_publish = models.ForeignKey(settings.AUTH_USER_MODEL,
                                    null=True, blank=True)
+    """User who last published this object (required)."""
+
     time_expire = models.DateTimeField(db_index=True, null=True, blank=True)
+    """:py:class:`datetime` for when this object expires."""
 
     objects = PassThroughManager().for_queryset_class(LiveQuerySet)()
-
 
     class Meta(object):
         abstract = True
